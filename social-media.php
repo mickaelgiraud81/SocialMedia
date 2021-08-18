0779ff4e63297be23151fa18881d422a34840212
BCaroline01 <76958265+BCaroline01@users.noreply.github.com>
2021-08-18 09:08:30 +0200

    initial commit

---
+++ social-media.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Plugin Name: Social Media
+ * Description: A simple CSS and SVG driven social icons widget.
+ * Version: 1.0
+ * Author: Caroline et Mickael
+ **/
+
+add_action("admin_menu", "SocialMedia_add_menu_dashboard");
+
+function SocialMedia_add_menu_dashboard()
+{
+
+    add_menu_page(
+        __("Social Media - settings", "SocialMedia"), // texte de la balise <title>
+        __("Social Media", "SocialMedia"),  // titre de l'option de menu
+        "manage_options", // droits requis pour voir l'option de menu
+        "SocialMedia_menu_admin", // slug
+        "SocialMedia_create_page_settings", // fonction de rappel pour créer la page
+        "dashicons-share"
+    );
+}
+
+
+function SocialMedia_create_page_settings()
+{
+
+    global $title;   // titre de la page du menu, tel que spécifié dans la fonction add_menu_page
+
+?>
+
+    <div class="wrap">
+
+        <h2><?php echo $title; ?></h2>
+
+        ...
+
+    </div>
+
+<?php
+
+}
