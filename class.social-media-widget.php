5b37245a215b8f047d93fb149cdcc324e71f8146
BCaroline01 <bony.caroline@gmail.com>
2021-08-18 15:29:24 +0200

    display widget

--- class.social-media-widget.php
+++ class.social-media-widget.php
@@ -2,6 +2,7 @@
 
 class SocialMedia_widget extends WP_Widget{
 
+// Main constructor
     public function __construct() {
         load_plugin_textdomain( 'SocialMedia');
         parent::__construct(
@@ -13,26 +14,48 @@ class SocialMedia_widget extends WP_Widget{
             );
     }
 
-
+// The widget form (for the backend )
     function form( $instance ) {
 
         $defaults = apply_filters( 'icones_styles', array(
-            'name'      => '',
-            'facebook'  => '',
-            'instagram' => '',
-            'youtube'   => '',
-            'linkedin'  => '',
+            'name_social'      => '',
+            'link_social'      => '',
+            'img_social'      => '',
 
         ));
         extract( wp_parse_args( ( array ) $instance, $defaults ) ); ?>
 
           <p>
-
-            <label for="<?php echo esc_attr( $this->get_field_id( 'name' )); ?>"><?php _e('name:', 'icones'); ?></label>
-            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'name' ) ); ?>"name="<?php echo esc_attr( $this->get_field_name( 'name')); ?>" type="text" value="<?php echo esc_attr($name); ?>"/>
-
-          
+            <label for="<?php echo esc_attr( $this->get_field_id( 'name_social' )); ?>"><?php _e('name:', 'icones'); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'name_social' ) ); ?>"name="<?php echo esc_attr( $this->get_field_name( 'name_social')); ?>" type="text" value="<?php echo esc_attr($name); ?>"/>
           </p>  
           <?php
+    }
+
+    // Display the widget
+    public function widget( $args, $instance ) {
+
+	extract( $args );
+
+	// Check the widget options
+	$name    = isset( $instance['name_social'] ) ? apply_filters( 'widget_title', $instance['name_social'] ) : '';
+
+	// WordPress core before_widget hook (always include )
+	echo $before_widget;
+
+   // Display the widget
+   echo '<div class="block_widget">';
+
+		// Display widget title if defined
+		if ( $name ) {
+			echo $before_title . $name . $after_title;
+		}
+
+
+	echo '</div>';
+
+	// WordPress core after_widget hook (always include )
+	echo $after_widget;
+
     }
 }
--- functions.php
+++ functions.php
@@ -4,13 +4,5 @@ add_action( 'widgets_init', 'new_social_zone' );
 
 function new_social_zone() {
 
-    register_sidebar( array(
-        'name'          => 'icone media réseaux sociaux',
-        'id'            => 'nouvelle_zone',
-        'before_widget' => '<div>',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h2 class="rounded">',
-        'after_title'   => '</h2>',
-        'class'         => 'icons_social',
-    ) );
+    register_widget( 'SocialMedia_widget');
 }
